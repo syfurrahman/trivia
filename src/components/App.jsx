e5bcd301180cf07750d404127ce81c8c770358fa
Syfur Rahman <43377197+syfurrahman@users.noreply.github.com>
2021-01-18 17:53:04 -0500

    Added Question Did the question component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,40 @@
 import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
+import Question from "./Question";
+
+let currentQ = 0;
 
 function App() {
   return <div className="app">Trivia!</div>;
+  //here i think//
 }
- 
-export default App;
+// function Question (){
+//   return (<div> </div>);
+// }
+// <div className="App">
+//   <div className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <h2>React Quiz</h2>
+//   </div>
+//   <Question content="What is your favourite food?" />
+// </div>
+
+import Question from "./Question";
+
+let currentQ = 0;
+
+function App() {
+  return <div className="app">Trivia!</div>;
+  //here i think//
+}
+// function Question (){
+//   return (<div> </div>);
+// }
+// <div className="App">
+//   <div className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <h2>React Quiz</h2>
+//   </div>
+//   <Question content="What is your favourite food?" />
+// </div>
