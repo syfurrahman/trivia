0896bd877524d806aa1cf586609bd71e73ed7ade
Syfur Rahman <43377197+syfurrahman@users.noreply.github.com>
2021-02-01 16:32:57 -0500

    trying to export the app along with rendering questions Changed the app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,16 +9,6 @@ function App() {
   return <div className="app">Trivia!</div>;
   //here i think//
 }
-// function Question (){
-//   return (<div> </div>);
-// }
-// <div className="App">
-//   <div className="App-header">
-//     <img src={logo} className="App-logo" alt="logo" />
-//     <h2>React Quiz</h2>
-//   </div>
-//   <Question content="What is your favourite food?" />
-// </div>
 
 import Question from "./Question";
 
