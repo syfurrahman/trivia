b03e46ef609bdf68b647c1054128fbc7aad97c29
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-02-02 12:13:28 -0500

    Did most of day 2 I finished up day 2 goal 1, but got stuck on goal 2.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,44 @@
 import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
-
 let questionNum = 0;
-
 function App() {
   return (
     <div className="app">
       <h1>Trivia!</h1>
       <Question question={data[questionNum].question} />
+      <Answer answers={data[questionNum].question} />
       <NextQuestion />
+      <CorrectAnswer />
     </div>
   );
 }
 
 function Question(props) {
   return <div>{props.question.text}</div>;
-  <Answer answers={data[questionNum].question} />;
 }
 
 function NextQuestion() {
-  return <button>NextQuestion</button>;
+  return <button>Next Question</button>;
+}
+
+function CorrectAnswer(props) {
+  let [isAnswered, setisAnswered] = useState(false);
+  return (
+    <button onClick={() => setIsAnswered(true)}>
+      Click for correct answer
+    </button>
+  );
 }
 
 function Answer(props) {
-  return <div>{props.answers.choices}</div>;
+  return (
+    <div>
+      {props.answers.choices.map((answer) => (
+        <div>{answer}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
